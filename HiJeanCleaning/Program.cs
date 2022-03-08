46c71c3af00fee277cb976f2cd7227cda1668fb7
Teagan Walton <teawalt@chbc.school.nz>
2022-03-09 09:41:38 +1300

    Component 1 -v1 -Welcome message

--- HiJeanCleaning/Program.cs
+++ HiJeanCleaning/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace HiJeanCleaning
 {
@@ -6,7 +7,21 @@ namespace HiJeanCleaning
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(
+                "Welcome to Hi-Jean Cleaning!\n"+
+                " You will enter sample of germs and the chemical you are wanting to test \n" +
+                "These tests will show the efficiency rating of each test you complete \n" +
+                "-------------------------------------------------------------------------\n\n" +
+                "Please enter the number of germs you are sampling with. This can be entered manually or randomly\n"+
+                "Enter 1 if you want to input manually or Enter 2 if you want a randomly generated number \n"+
+                "-----------------------------------------------------------------------------------------");
+
+          
+        
+        
         }
+       
     }
+
+
 }
