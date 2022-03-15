f9ff1f2d736820740a507b0d96d866f869d48765
Teagan Walton <teawalt@chbc.school.nz>
2022-03-16 09:21:18 +1300

    component 1- v4 -efficiency rate added -test looped 5 times

--- HiJeanCleaning/Program.cs
+++ HiJeanCleaning/Program.cs
@@ -7,6 +7,7 @@ namespace HiJeanCleaning
     {
         static void Main(string[] args)
         {
+            //Welcome message
             Console.WriteLine(
                 "Welcome to Hi-Jean Cleaning!\n" +
                 "You will enter sample of germs and the chemical you are wanting to test \n" +
@@ -18,34 +19,46 @@ namespace HiJeanCleaning
         
         
         }
+
         static void TestChem()
         {
-           
-           
-            Console.WriteLine("Enter amount of Germs you would like to sample with");
-            float liveGerm = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture.NumberFormat);
-           
-        
-
-            Console.WriteLine("Please choose a chemical you are wanting to test with from the list:\n"+
-                "1. Formaldehyde \n" +
-                "2. Ethanol \n" +
-                "3. Hydrogen Peroxide \n" +
-                "4. Chlorine \n" +
-                "5. Peracetic Acid \n"
-                );
+            //chemical list 
+            Console.WriteLine("Please choose a chemical you are wanting to test with from the list:\n" +
+              "1. Formaldehyde \n" +
+              "2. Ethanol \n" +
+              "3. Hydrogen Peroxide \n" +
+              "4. Chlorine \n" +
+              "5. Peracetic Acid \n"
+              );
 
             int chemChoice = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Please enter the remaining number of germs in sample");
+            //for loop for repeating the test 5 times
+            for (int i = 1; i < 6; i++)
+            {
+                Console.WriteLine($"Enter amount of Germs you would like to sample with: Test {i}");
+                float liveGerm = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture.NumberFormat);
 
-            float leftGerms = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture.NumberFormat);
+                Console.WriteLine(
+                    "---Please wait 30 mins before entering amount of live germs left---\n" +
+                    "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
-            float effRate = (liveGerm - leftGerms) / 30;
+                Console.WriteLine("Please enter the remaining number of germs in sample");
 
-            Console.WriteLine($"{effRate}");
+                float leftGerms = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture.NumberFormat);
+
+                //eficiency rate
+                float effRate = (liveGerm - leftGerms) / 30;
+
+                Console.WriteLine(
+                    $"The efficency rate of chemcical {chemChoice} is {effRate} \n" +
+                    "--------------------------------------------------------------------- \n");
+
+            }
+        
+
+        
         }
-   
     
     }
 
